3e373e960c5688a2443c2f14a82d15f91e952805
Hyacine Alnuma <hyacinealnuma@gmail.com>
2023-09-14 10:08:59 +0200

    addTrick route set up

--- src/Controller/TrickController.php
+++ src/Controller/TrickController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Trick;
 use App\Repository\TrickRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\TrickType;
 
 class TrickController extends AbstractController
 {
@@ -26,6 +29,25 @@ class TrickController extends AbstractController
 
         return $this->render('pages/trick/index.html.twig', [
             'trick' => $trick
+        ]);
+    }
+
+    #[Route('/addTrick', name: 'addTrick')]
+    public function addTrick(Request $request, EntityManagerInterface $entityManager)
+    {
+        $trick = new Trick();
+
+        $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($trick);
+            $entityManager->flush();
+        }    
+
+        return $this->render('pages/addTrick/index.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
